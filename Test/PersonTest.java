a68465675aac6d5073ab7b533e30368089666875
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-08 11:56:57 -0400

    Lab0

--- Test/PersonTest.java
+++ Test/PersonTest.java
@@ -13,23 +13,35 @@ public class PersonTest {
 
   private Person john;
 
+  /**
+   * Create a Person class
+   */
   @Before
   public void setUp() {
 
     john = new Person("John", "Doe", 1945);
   }
 
+  /**
+   * Test if the first name matches
+   */
   @Test
   public void testFirst() {
     assertEquals("John", john.getFirstName());
 
   }
 
+  /**
+   * Test if the second name matches
+   */
   @Test
   public void testSecond() {
     assertEquals("Doe", john.getLastName());
   }
 
+  /**
+   * Test if the year of Birth matches
+   */
   @Test
   public void testYearOfBirth() {
     assertEquals(1945, john.getYearOfBirth());
