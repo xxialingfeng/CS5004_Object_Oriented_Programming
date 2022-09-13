ce412433a6e858ebf7d706c72e47b37984976b97
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-13 12:57:20 -0400

    lab01

--- Test/stock/StockTest.java
+++ Test/stock/StockTest.java
@@ -12,32 +12,50 @@ public class StockTest {
 
   private Stock apple;
 
+  /**
+   * Create a stock class
+   */
   @Before
   public void setUp() {
     apple = new Stock("Computer", "Apple", 200.0);
     apple.setCurrentPrice(300);
   }
 
+  /**
+   * Test if symbol of stock matches
+   */
   @Test
   public void testSymbol() {
     assertEquals("Computer", apple.getSymbol());
   }
 
+  /**
+   * Test if name of stock matches
+   */
   @Test
   public void testName() {
     assertEquals("Apple", apple.getName());
   }
 
+  /**
+   * Test if cost basis matches
+   */
   @Test
   public void testCostBasis() {
     assertEquals(200.0, apple.getCostBasis(), 0.00);
   }
 
+  /**
+   * Test if current price matches
+   */
   @Test
   public void getCurrentPrice() {
     assertEquals(300, apple.getCurrentPrice(),0.000);
   }
 
+  /**
+   * Test if stock can be set new cost basis
+   */
   @Test
   public void testSetCostBasis() {
     Stock stockOne = new Stock("Phone", "Apple", 200);
@@ -45,6 +63,9 @@ public class StockTest {
     assertEquals(250, stockOne.getCostBasis(), 0.000);
   }
 
+  /**
+   * Test if change percent matches
+   */
   @Test
   public void testGetChangePercent() {
     Stock stockTwo = new Stock("TV", "Dell", 500);
@@ -53,7 +74,9 @@ public class StockTest {
     assertEquals(0.2, stockTwo.getChangePercent(), 0.000);
   }
 
-
+  /**
+   * Test if to String matches
+   */
   @Test
   public void testToString() {
     Stock stockThree = new Stock("Clothes", "NorthFace", 300);
