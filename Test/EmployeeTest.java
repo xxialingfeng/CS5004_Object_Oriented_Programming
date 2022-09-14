ae8e6b10ec34411bbf1f3fd93e84ce7be87176bd
xialingfeng <lingfengxia.jake@gmail.com>
2022-09-14 11:09:33 -0400

    hw1

---
+++ Test/EmployeeTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**.
+ * This is a text class Employee
+ */
+public class EmployeeTest {
+
+  /**.
+   * Construct a personA object to test if work hours can be added.
+   */
+  @Test
+  public void addHoursWorked() {
+    Employee personA = new Employee("Jeff", "SUPS-111", 3.25);
+    personA.addHoursWorked(2.5);
+    assertEquals(personA.getHoursWorked(),2.5,0.000);
+  }
+
+  /**
+   * Construct a personB object to test if work hours can be reset.
+   */
+  @Test
+  public void resetHoursWorked() {
+    Employee personB = new Employee("Zoey", "SUPS-112", 2.6);
+    personB.addHoursWorked(3);
+    personB.resetHoursWorked();
+    assertEquals(personB.getHoursWorked(), 0, 0.000);
+  }
+
+  /**.
+   * Construct a personC object to test if we can get how many hours he/she has worked.
+   */
+  @Test
+  public void getHoursWorked() {
+    Employee personC = new Employee("Connor", "SUPS-113", 3.8);
+    personC.addHoursWorked(2.5);
+    assertEquals(2.5,personC.getHoursWorked(), 0.000);
+  }
+
+  /**
+   * Construct a personD object to test the string representation is the same format as expected.
+   */
+  @Test
+  public void testToString() {
+    Employee personD = new Employee("Nina", "SUPS-114", 5.6);
+    personD.addHoursWorked(15);
+    assertEquals("Name: Nina\nID: SUPS-114\nPayment after taxes: $ 75.60", personD.toString());
+  }
+}
--- Test/PaycheckTest.java
+++ Test/PaycheckTest.java
@@ -1,13 +1,42 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**.
+ * This is a test class for paycheck
+ */
 public class PaycheckTest {
 
+  /**
+   * Test the string representation is the same format as expected.
+   */
   @Test
   public void testToString() {
     Paycheck pc = new Paycheck(0.005,1);
     assertEquals("Payment after taxes: $ 0.01", pc.toString());
+    pc = new Paycheck(10.5,40);
+    assertEquals("Payment after taxes: $ 357.00", pc.toString());
+  }
 
+  /**
+   * Test if the payment after taxes matches.
+   */
+  @Test
+  public void getPayAfterTaxes() {
+    Paycheck pc = new Paycheck(3.4,5);
+    assertEquals(15.3,pc.getPayAfterTaxes(),0.000);
+    pc = new Paycheck(0.002, 3);
+    assertEquals(0.01,pc.getPayAfterTaxes(), 0.000);
+  }
+
+  /**
+   * Test if total payment matches.
+   */
+  @Test
+  public void getTotalPay() {
+    Paycheck pc = new Paycheck(0.04,32);
+    assertEquals(1.28, pc.getTotalPay(), 0.000);
+    pc = new Paycheck(15, 29);
+    assertEquals(435.0, pc.getTotalPay(),0.000);
   }
 }
